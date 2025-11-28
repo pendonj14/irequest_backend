130e315b956522d9a746850112dc1c4ecf0b066b
pendonj14 <pendon.josephjr@gmail.com>
2025-11-28 23:17:19 +0800

    fuck it

--- backend/settings.py
+++ backend/settings.py
@@ -157,9 +157,3 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOWED_ORIGINS = [
-    "https://irequest-nu.vercel.app/",
-    "https://irequest-nu.vercel.app",
-    "https://irequest-nu.vercel.app/api"
-]
