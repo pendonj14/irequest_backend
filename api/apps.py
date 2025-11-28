cdcfa8b9b65ad4b3f751be5956b45a3283ec0c4f
pendonj14 <pendon.josephjr@gmail.com>
2025-11-28 22:10:52 +0800

    yawyaywa

--- api/apps.py
+++ api/apps.py
@@ -4,8 +4,3 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
-
-    def ready(self):
-        from django.contrib.auth.models import User
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'admin@example.com', 'gwapoko123')
--- backend/settings.py
+++ backend/settings.py
@@ -156,4 +156,9 @@ MEDIA_URL = '/media/'
 # Path where media is stored on your computer
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://irequest-nu.vercel.app/login",
+    "https://irequest-nu.vercel.app",
+    "https://irequest-nu.vercel.app/"
+]
