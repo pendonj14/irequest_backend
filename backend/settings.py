0092db25664c91f734ef5861f9a89574fdcc8bd6
pendonj14 <pendon.josephjr@gmail.com>
2025-11-28 19:40:18 +0800

    database kuno

--- backend/settings.py
+++ backend/settings.py
@@ -101,12 +101,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'backend',
-        'USER': 'joseph',
-        'PASSWORD': 'joseph',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
